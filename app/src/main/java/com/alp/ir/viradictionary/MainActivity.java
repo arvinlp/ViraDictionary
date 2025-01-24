5593861b77f4101737eb046b123f3ecd13df6974
arvinlp <arvinlp91@gmail.com>
2025-01-24 22:25:47 +0330

    updated

--- app/src/main/java/com/alp/ir/viradictionary/MainActivity.java
+++ app/src/main/java/com/alp/ir/viradictionary/MainActivity.java
@@ -1,19 +1,22 @@
 package com.alp.ir.viradictionary;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
@@ -21,16 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapter;
     private ArrayList<String> words;
+    private ArrayList<String[]> wordDetails; // لیست برای ذخیره جزئیات هر کلمه
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
         EditText searchField = findViewById(R.id.searchField);
         ListView listView = findViewById(R.id.listView);
 
         words = new ArrayList<>();
+        wordDetails = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
         listView.setAdapter(adapter);
 
@@ -57,16 +65,30 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
+
+        // تنظیم کلیک روی آیتم‌های لیست
+        listView.setOnItemClickListener((AdapterView<?> parent, android.view.View view, int position, long id) -> {
+            // گرفتن اطلاعات کلمه انتخابی
+            String[] details = wordDetails.get(position);
+            String enWord = details[1];
+
+            // ارسال اطلاعات به WordDetailsActivity
+            Intent intent = new Intent(MainActivity.this, WordDetailsActivity.class);
+            intent.putExtra("en_word", enWord);
+            startActivity(intent);
+        });
     }
 
     // متد برای بارگذاری کلمات از پایگاه داده
     private void loadWords(SQLiteDatabase db, String query) {
         words.clear();
+        wordDetails.clear();
 
         String sql = "SELECT fa, en FROM words";
         if (!query.isEmpty()) {
             sql += " WHERE fa LIKE ? OR en LIKE ?";
         }
+        sql += " GROUP BY en ";// گروه بندی کلمات یکسان
 
         Cursor cursor = db.rawQuery(sql, query.isEmpty() ? null : new String[]{"%" + query + "%", "%" + query + "%"});
 
@@ -74,11 +96,42 @@ public class MainActivity extends AppCompatActivity {
             do {
                 String fa = cursor.getString(0);
                 String en = cursor.getString(1);
-                words.add(fa + " - " + en);
+                words.add(en);
+                wordDetails.add(new String[]{fa, en});
             } while (cursor.moveToNext());
         }
         cursor.close();
 
         adapter.notifyDataSetChanged();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // متصل کردن منو به Toolbar
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // پیدا کردن آیتم جستجو
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // مدیریت رویداد جستجو
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // اعمال جستجو هنگام تایید
+                loadWords(new DatabaseHelper(MainActivity.this).openDatabase(), query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // اعمال جستجو هنگام تایپ
+                loadWords(new DatabaseHelper(MainActivity.this).openDatabase(), newText);
+                return true;
+            }
+        });
+
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/alp/ir/viradictionary/WordDetailsActivity.java
@@ -0,0 +1,70 @@
+package com.alp.ir.viradictionary;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WordDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_details);
+
+        // پیکربندی Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // فعال کردن دکمه بازگشت
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        // گرفتن اطلاعات از Intent
+        String enWord = getIntent().getStringExtra("en_word");
+
+        // اتصال به پایگاه داده
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        dbHelper.checkAndCopyDatabase();
+        SQLiteDatabase db = dbHelper.openDatabase();
+
+        // اجرای کوئری برای بازیابی اطلاعات بیشتر
+        String sql = "SELECT fa, en FROM words WHERE en = ?";
+        Cursor cursor = db.rawQuery(sql, new String[]{enWord});
+
+        StringBuilder faWord = new StringBuilder();
+        if (cursor.moveToFirst()) {
+            boolean flag = false;
+            do {
+                if(flag) faWord.append(" - "); // فاصله بین دو معنا
+                faWord.append(cursor.getString(0)); // مقدار ستون fa
+                flag = true;
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        // نمایش اطلاعات در TextView ها
+        TextView faTextView = findViewById(R.id.faTextView);
+        TextView enTextView = findViewById(R.id.enTextView);
+
+        if (faWord != null) {
+            faTextView.setText(faWord.toString());
+            enTextView.setText(enWord);
+        } else {
+            faTextView.setText("کلمه‌ای پیدا نشد.");
+            enTextView.setText(enWord);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // برگشت به صفحه قبلی
+        onBackPressed();
+        return true;
+    }
+}
